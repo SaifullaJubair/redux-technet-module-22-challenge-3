48cc4128063f83cd7e1dae27219bdb731bfe5bdb
SaifullaJubair <saifullajubair6@gmail.com>
2024-01-23 01:07:01 +0600

    refetch  tagtypes add

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  tagTypes: ['comments'],
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => '/products',
@@ -11,12 +12,17 @@ export const api = createApi({
       // query: (id) => ({ url: `/product/${id}` }),
       query: (id) => `/product/${id}`,
     }),
+    getComment: builder.query({
+      query: (id) => `/comment/${id}`,
+      providesTags: ['comments'],
+    }),
     postComment: builder.mutation({
       query: ({ id, data }) => ({
         url: `/comment/${id}`,
         method: 'POST',
         body: data,
       }),
+      invalidatesTags: ['comments'],
     }),
   }),
 });
@@ -25,4 +31,5 @@ export const {
   useGetProductsQuery,
   useSingleProductQuery,
   usePostCommentMutation,
+  useGetCommentQuery,
 } = api;
