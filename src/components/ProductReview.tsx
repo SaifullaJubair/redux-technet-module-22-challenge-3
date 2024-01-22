33075347d4024679e7b8534d88e7bc8309b66de0
SaifullaJubair <saifullajubair6@gmail.com>
2024-01-23 00:40:05 +0600

    comment add in server

--- src/components/ProductReview.tsx
+++ src/components/ProductReview.tsx
@@ -3,6 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
 import { FiSend } from 'react-icons/fi';
+import { usePostCommentMutation } from '@/redux/api/apiSlice';
 
 const dummyComments = [
   'Bhalo na',
@@ -18,9 +19,21 @@ interface IProps {
 export default function ProductReview({ id }: IProps) {
   const [inputValue, setInputValue] = useState<string>('');
   console.log(inputValue);
+
+  const [postComment, { isLoading, isSuccess, isError }] =
+    usePostCommentMutation();
+  console.log(isLoading);
+  console.log(isSuccess);
+  console.log(isError);
+
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
+    console.log(inputValue);
+    const options = {
+      id: id,
+      data: { comment: inputValue },
+    };
+    postComment(options);
     setInputValue('');
   };
 
--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { da } from 'date-fns/locale';
 
 export const api = createApi({
   reducerPath: 'api',
