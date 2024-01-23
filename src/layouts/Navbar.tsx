66e5847dfcf9a1ce4568b9f3102b8fcdcfb4e5cb
SaifullaJubair <saifullajubair6@gmail.com>
2024-01-23 19:59:04 +0600

    onAuthStateChange

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -12,8 +12,15 @@ import {
 import { HiOutlineSearch } from 'react-icons/hi';
 import Cart from '../components/Cart';
 import logo from '../assets/images/technet-logo.png';
+import { useAppSelector } from '@/redux/hook';
+import { LogOut } from 'lucide-react';
 
 export default function Navbar() {
+  const { user } = useAppSelector((state) => state.user);
+
+  const handleLogut = () => {
+    console.log(LogOut);
+  };
   return (
     <nav className="w-full h-16 fixed top backdrop-blur-lg z-10">
       <div className="h-full w-full bg-white/60">
@@ -60,15 +67,24 @@ export default function Navbar() {
                     <DropdownMenuItem className="cursor-pointer">
                       Profile
                     </DropdownMenuItem>
-                    <DropdownMenuItem className="cursor-pointer">
-                      Billing
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className="cursor-pointer">
-                      Team
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className="cursor-pointer">
-                      Subscription
-                    </DropdownMenuItem>
+                    {!user.email && (
+                      <>
+                        <DropdownMenuItem className="cursor-pointer">
+                          <Link to="/login">Login</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="cursor-pointer">
+                          <Link to="/signup">SignUp</Link>
+                        </DropdownMenuItem>
+                      </>
+                    )}
+                    {user.email && (
+                      <DropdownMenuItem
+                        className="cursor-pointer"
+                        onClick={handleLogut}
+                      >
+                        Logout
+                      </DropdownMenuItem>
+                    )}
                   </DropdownMenuContent>
                 </DropdownMenu>
               </li>
